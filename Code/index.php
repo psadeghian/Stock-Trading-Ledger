bf66e563517b6ca073409a802428ff24328a3db5
psadeghian <Pasha@Pashas-Mac.local>
2016-12-05 08:08:29 -0500

    Changed styling and added a welcome page.

---
+++ Code/index.php
@@ -0,0 +1,9 @@
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php
+require "menu.php";
+echo "<div id='welcome'><p>Welcome user! <br> Please select from the list above.</p></div>";
+?>
+</body>
