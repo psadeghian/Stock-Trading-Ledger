4019f2b2e7165099c9e85d8709ba62b116cd9049
psadeghian <Pasha@Pashas-Mac.local>
2016-12-06 01:22:34 -0500

    Minor html change.

--- Code/menu.php
+++ Code/menu.php
@@ -3,5 +3,5 @@
 <li><a href="acctlist.php">Accounts</a></li>
 <li><a href="acctassnlist.php">Account Associations</a></li>
 <li><a href="stocklist.php?order_by=ticker">Stocks</a></li>
-<li><a href="maketransactionhtm.php">Record A Transaction</a></li>
+<li><a href="maketransactionhtm.php">Record a Transaction</a></li>
 </ul>
